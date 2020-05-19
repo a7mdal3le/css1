dcbfaff43cfabd39a6f55a23facb52046babf25f
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-19 13:01:40 +0300

    Create mskchat.js

---
+++ mskchat.js
@@ -0,0 +1,14 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<img src="/sico/z1e8gf0c0610.png" class="fr" style="width:100%;margin-top: 0px;" >
+<a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 42);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="mobile.html"> التطبيق </a>
+<a id="des22" style="width:95px;margin:-68px 2px 2px;float:right;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 15);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="sub.html">الأشتراكات</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://www.al3nood.net/"> شات العنود </a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://mskchat.com/">شات الجوال</a>
+<a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="contact.html"> اتصل بنا </a>
+<a id="des22" style="color: #ffffff;width:97px;margin:2px;transform:matrix(1, 0, 0, 1, 8, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="rules.html">القوانين</a>
+`).insertBefore('.nav-tabs');
+$(`<font size="4" color="#ffffff"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:#A17082;">نهنئكم بحلول شهر رمضان المبارك , اعاده الله علينا وعليكم وعلى الامه الاسلاميه بالخير واليمن والبركات , وتقبل الله صيامكم وقيامكم , الادارهـ 🌙 
+</marquee></font>`).insertBefore('.nav-tabs'); 
+eval((close+'').replace('location.reload()','location.href="/"'));
+$.getScript("https://cdn.statically.io/gh/a7mdal3le/7ob/806aab4c/mskchat.js");
+vchat=true;
